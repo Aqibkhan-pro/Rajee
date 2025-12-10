7dd79b1d00b1a91216c79797dfe535c387ac4454
Anees Ahmad <anees@4iisolutions.com>
2025-12-10 12:29:43 +0500

    #preowned devices realted changes done and ios folder added.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Preowned-Devices',
+  appId: 'app.preowned.devices',
+  appName: 'Preowned Devices',
   webDir: 'www'
 };
 
