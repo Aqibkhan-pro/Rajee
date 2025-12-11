53673d9717acf2e4ddc9d46dff6da0576636cc42
Anees Ahmad <anees@4iisolutions.com>
2025-12-11 17:08:20 +0500

    --lazy loading related changes done

--- src/app/main/parts-requested/parts-requested.component.ts
+++ src/app/main/parts-requested/parts-requested.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from "@ionic/angular";
 
 @Component({
   selector: 'app-parts-requested',
   templateUrl: './parts-requested.component.html',
   styleUrls: ['./parts-requested.component.scss'],
+  imports: [IonicModule],
 })
 export class PartsRequestedComponent  implements OnInit {
 
