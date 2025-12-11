cc0358df17100ceb3ceb27ed872f21c9c5c40de4
Anees Ahmad <anees@4iisolutions.com>
2025-12-11 16:27:02 +0500

    -- Lazy loading module added given-devices, parts-requested, repairs, and received devices.

---
+++ src/app/main/given-devices/given-devices.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GivenDevicesRoutingModule } from './given-devices-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GivenDevicesRoutingModule
+  ]
+})
+export class GivenDevicesModule { }
--- src/app/shared/utils/app-routes.ts
+++ src/app/shared/utils/app-routes.ts
@@ -1,8 +1,12 @@
-export const ROUTES = {
+export const APP_ROUTES = {
   STARTED: 'stated',
   AUTH: 'auth',
   LOGIN: 'login',
   FORGET_PASSWORD: 'forget-password',
   DASHBOARD: 'dashboard',
   MAIN: 'main',
+  GIVEN_DEVICES: 'given-devices',
+  RECEIVED_DEVICES: 'received-devices', 
+  PARTS_REQUESTED : 'parts-requested',
+  REPAIRS: 'repairs',
 };
