aba933bdb5458275592e462dc138f1e27d4ac543
Anees Ahmad <anees@4iisolutions.com>
2025-12-12 11:07:30 +0500

    --global header component changes done

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+// Components
+import { GlobalSearchComponent } from './components/global-search/global-search.component';
+import { GlobalHeaderComponent } from './components/global-header/global-header.component';
+
+const COMPONENTS = [
+  GlobalSearchComponent,
+  GlobalHeaderComponent,
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...COMPONENTS,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    ...COMPONENTS,
   ]
 })
 export class SharedModule { }
