d967684a1c628e2452c78928cf38ebe0beb4748d
arslan <marslan@4iisolutions.com>
2025-12-12 14:57:01 +0500

    --Slider Implementation

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -15,6 +15,12 @@ export class DashboardComponent implements OnInit {
   routes = APP_ROUTES;
   segment: Segment = Segment.REPAIR;
   SegmentEnum = Segment;
+
+  images = [
+    'https://images.unsplash.com/photo-1569908420024-c8f709b75700?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1569908420024-c8f709b75700?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1569908420024-c8f709b75700?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  ];
   constructor(
     private router: Router,
     private navCtrl: NavController,
