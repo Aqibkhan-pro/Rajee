0a9948cf84d015f3a93a0df1c49ea28db1532236
= <=>
2025-12-12 09:51:11 +0500

    dashboardd card

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController, PopoverController } from '@ionic/angular';
+import { AuthService } from 'src/app/auth/services/auth.service';
 import { APP_ROUTES } from 'src/app/shared/utils/app-routes';
 
 @Component({
@@ -12,8 +13,9 @@ import { APP_ROUTES } from 'src/app/shared/utils/app-routes';
 export class DashboardComponent implements OnInit {
   routes = APP_ROUTES
   constructor(
-    private navCtrl : NavController,
     private router: Router,
+    private navCtrl : NavController,
+    private authService : AuthService,
     private popOverCtrl : PopoverController) { }
 
   ngOnInit() {
@@ -29,4 +31,28 @@ export class DashboardComponent implements OnInit {
   goTo(path: string) {
   this.router.navigate([APP_ROUTES.MAIN+'/'+path]);
 }
+
+count = 1
+async changeTheme() {
+  // if (await this.mediaPermissionService.scannerPermission()) {
+  //   this.navCtrl.navigateForward(['main/scan']);
+  // }
+  // else {
+  //   console.log('Camera permission is required to access the scanner.');
+  // }
+  this.count++;
+  if (this.count == 1) {
+    this.authService.changeTheme('theme-blue')
+  }
+  else if (this.count == 2) {
+    this.authService.changeTheme('theme-dark')
+  } else if(this.count ==3){
+    this.authService.changeTheme('theme-darkblue')
+  }
+  else {
+    this.authService.changeTheme('theme-light')
+    this.count = 0
+  }
+}
+
 }
