1443e310d42df5b027d2e03f73ec8aeb43128616
Anees Ahmad <anees@4iisolutions.com>
2025-12-15 13:12:47 +0500

    - parts lsit ui design completed.

--- src/app/main/received-devices/parts-list/parts-list.component.ts
+++ src/app/main/received-devices/parts-list/parts-list.component.ts
@@ -7,9 +7,74 @@ import { Component, OnInit } from '@angular/core';
   standalone:false
 })
 export class PartsListComponent  implements OnInit {
-
+ devices: Device[] = [
+    {
+      imei: '343434788348343XXX',
+      title: 'iPhone 15 Pro Max',
+      description: 'Grade B',
+      status: 'completed',
+      time: '2 hrs ago'
+    },
+    {
+      imei: '343434788348343XXX',
+      title: 'iPhone 14 Pro',
+      description: 'Grade B',
+      status: 'pending',
+      time: '4 hrs ago'
+    },
+    {
+      imei: '343434788348343XXX',
+      title: 'iPhone 12',
+      description: 'Grade B',
+      status: 'completed',
+      time: '1 day ago'
+    },
+    {
+      imei: '343434788348343XXX',
+      title: 'iPhone 11 Pro',
+      description: 'Grade B',
+      status: 'completed',
+      time: '2 days ago'
+    },
+    {
+      imei: '343434788348343XXX',
+      title: 'iPhone 7',
+      description: 'Grade B',
+      status: 'pending',
+      time: '4 hrs ago'
+    },
+     {
+      imei: '343434788348343XXX',
+      title: 'iPhone 10 XS MAX',
+      description: 'Grade B',
+      status: 'completed',
+      time: '2 days ago'
+    },
+    {
+      imei: '343434788348343XXX',
+      title: 'iPhone 8',
+      description: 'Grade B',
+      status: 'returned',
+      time: '4 hrs ago'
+    },
+    {
+      imei: '343434788348343XXX',
+      title: 'iPhone 13 mini',
+      description: 'Grade B',
+      status: 'returned',
+      time: '4 hrs ago'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {}
 
+}
+
+interface Device {
+  imei: string;
+  title: string;
+  description: string;
+  status: 'completed' | 'pending'| 'returned';
+  time: string;
 }
